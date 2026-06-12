60972a82b2d83330ef5876d3cbc2bdb084230798
chandelnitin <128496532+chandelnitin@users.noreply.github.com>
2026-06-12 22:51:29 +0530

    Time: 6 ms (39.87%) | Memory: 28.5 MB (60.79%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:  // agr tm array ko double kr do or fir next greater ka logic lgao to chlege
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+         stack<int>st;
+         vector<int>ans(n,-1);
+         for(int i=0;i<2*n;i++){
+            while(!st.empty() && nums[st.top()]<nums[i%n]){
+                ans[st.top()]=nums[i%n];
+                st.pop();
+            }
+
+            st.push(i%n);
+         }
+
+         return ans;
+    }
+};
