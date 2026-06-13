6899e655ab6e899a7a7a9067761e4ce57bec5fa7
chandelnitin <128496532+chandelnitin@users.noreply.github.com>
2026-06-13 13:23:27 +0530

    Time: 11 ms (87.46%) | Memory: 26 MB (97.09%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -1,16 +1,40 @@
-class Solution {
-public: //3 approach
-    string removeStars(string s) {
-        string ans=""; // ans sring as stack using
-        for(int i=0;i<s.size();i++){
-           if(!ans.empty() && s[i]=='*'){
-            ans.pop_back();
-           }
-           else {
-              ans.push_back(s[i]);
-           }
-        }
+// class Solution {
+// public: //3 approach
+//     string removeStars(string s) {
+//         string ans=""; // ans sring as stack using
+//         for(int i=0;i<s.size();i++){
+//            if(!ans.empty() && s[i]=='*'){
+//             ans.pop_back();
+//            }
+//            else {
+//               ans.push_back(s[i]);
+//            }
+//         }
 
-        return ans;
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+
+class Solution {
+public: //3 approach  // Sc o(1)
+    string removeStars(string s) {
+        int j=0;
+        for(auto &ch:s){
+            if(ch=='*'){
+                j--;
+            }
+            else{
+                s[j++]=ch;
+            }
+        }
+        s.resize(j);
+        return s;
+   
     }
 };
