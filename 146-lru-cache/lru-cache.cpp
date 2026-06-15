b98a3374f8e942b1f6f645c430f2f3bd45ac49c6
chandelnitin <128496532+chandelnitin@users.noreply.github.com>
2026-06-16 00:27:02 +0530

    Time: 103 ms (27.81%) | Memory: 182.1 MB (32.40%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -11,29 +11,29 @@ public:
         if(mp.find(key)==mp.end()) return -1;// agr vaalue hai hi nhi
 
  // agr hai to usko least recently used bnao
-        int value=mp[key].first;
-        dl.erase(mp[key].second);
-        dl.push_back(key);
+        int value=mp[key].first; 
+        dl.erase(mp[key].second); // delte kro us node ko or usko least recently bnao
+        dl.push_back(key);// jo least recently hoga vo list ke last me hoga
         mp[key].second= prev(dl.end());
         return value;
     }
     
     void put(int key, int value) {
        if(mp.find(key)!=mp.end()){ //vo value phalse se hai usko update krna hai or least 
-            dl.erase(mp[key].second);
+            dl.erase(mp[key].second); // recently bnana hai
             dl.push_back(key);
-            mp[key].second=prev(dl.end());
-            mp[key].first=value;
+            mp[key].second=prev(dl.end());// prev list ke last element ka adress de raha
+            mp[key].first=value; // value update ki 
             return;
         }
 
         if(n==dl.size()){
-            int oldkey = dl.front();
-            dl.pop_front();
-            mp.erase(oldkey);
-        }
-
-        dl.push_back(key);
+            int oldkey = dl.front(); //hmara size ki capecity puri ho gyi isliye last
+            dl.pop_front(); // jo sbse km use hua hai use dlete krkenge . or vo hai list
+            mp.erase(oldkey);// ke start me  lekin list se delete krne se phale uski key
+        }// store krayenge kuki us key coorseponding entry mp me se bhi to delete krni 
+        // isiliye list me hmne key stor kiya hai
+        dl.push_back(key); 
         mp[key]= {value,prev(dl.end())};
         
     }
