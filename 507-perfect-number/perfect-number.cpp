da4e111462d8aa1397255cb0471367a9a921faa6
chandelnitin <128496532+chandelnitin@users.noreply.github.com>
2026-07-17 22:50:17 +0530

    Time: 1 ms (61.75%) | Memory: 7.8 MB (56.69%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -1,13 +1,34 @@
+// class Solution {
+// public:
+//     bool checkPerfectNumber(int n) {
+//         int sum=0;
+//         for(int i=1;i<n;i++){
+//             if(n%i==0){
+//                 sum+=i;
+//             }
+//         }
+
+//         return sum==n;
+//     }
+// };
+
+
+
+
+
+
 class Solution {
 public:
     bool checkPerfectNumber(int n) {
         int sum=0;
-        for(int i=1;i<n;i++){
+        for(int i=1;i*i<n;i++){
             if(n%i==0){
                 sum+=i;
+                sum+=n/i;
             }
         }
+        
 
-        return sum==n;
+        return sum-n==n;
     }
 };
