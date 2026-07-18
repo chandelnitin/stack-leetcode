b3409288f85013dfa4346d72bf6834dc36826bbf
chandelnitin <128496532+chandelnitin@users.noreply.github.com>
2026-07-18 15:19:06 +0530

    Time: 2 ms (3.93%) | Memory: 8.5 MB (8.61%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    int fun(int n,vector<int>&dp){
+        if(n==0) return 0;
+        if(n==1 || n==2) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=fun(n-1,dp) +fun(n-2,dp)+fun(n-3,dp);
+        
+    }
+public:
+    int tribonacci(int n) {
+        vector<int>dp(n+1,-1);
+        return fun(n,dp);
+
+    }
+};
