21ccf670a748449c704fe3a3db2082c21bd897e4
chandelnitin <128496532+chandelnitin@users.noreply.github.com>
2026-07-19 15:07:53 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (64.37%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,79 @@
+// class Solution {
+// public:
+//     int fun(int idx,vector<int>&nums,vector<int>&dp){
+//         if(idx==0) return nums[idx];
+//         if(idx<0) return 0;
+//         if(dp[idx]!=-1) return dp[idx];
+//         int notake= fun(idx-1,nums,dp);
+//         int take= nums[idx]+fun(idx-2,nums,dp);
+//         return dp[idx]= max(take,notake);
+        
+//     }
+//     int rob(vector<int>& nums) {
+//         vector<int>dp(nums.size(),-1);
+//         return fun(nums.size()-1,nums,dp);
+//     }
+// };
+
+
+
+
+
+
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>1 index shifting solution<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+
+
+// class Solution {
+// public:
+//     int fun(int idx,vector<int>&nums,vector<int>&dp){
+//         if(idx==1) return nums[0];
+//         if(idx==0) return 0;
+//         if(dp[idx]!=-1) return dp[idx];
+//         int notake= fun(idx-1,nums,dp);
+//         int take= nums[idx-1]+fun(idx-2,nums,dp);
+//         return dp[idx]= max(take,notake);
+        
+//     }
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int>dp(n+1,-1);
+//         return fun(n,nums,dp);
+//     }
+// };
+
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>tabulation <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
 class Solution {
 public:
-    int fun(int idx,vector<int>&nums,vector<int>&dp){
-        if(idx==0) return nums[idx];
-        if(idx<0) return 0;
-        if(dp[idx]!=-1) return dp[idx];
-        int notake= fun(idx-1,nums,dp);
-        int take= nums[idx]+fun(idx-2,nums,dp);
-        return dp[idx]= max(take,notake);
+    // int fun(int idx,vector<int>&nums,vector<int>&dp){
+    //     if(idx==1) return nums[0];
+    //     if(idx==0) return 0;
+    //     if(dp[idx]!=-1) return dp[idx];
+    //     int notake= fun(idx-1,nums,dp);
+    //     int take= nums[idx-1]+fun(idx-2,nums,dp);
+    //     return dp[idx]= max(take,notake);
         
-    }
+    // }
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
-        return fun(nums.size()-1,nums,dp);
+        int n=nums.size();
+        vector<int>dp(n+1,-1);
+        dp[0]=0;  dp[1]=nums[0];
+
+        for(int idx=2;idx<n+1;idx++){
+            int notake= dp[idx-1];
+            int take= nums[idx-1]+dp[idx-2];
+            dp[idx]=max(notake,take);
+        }
+
+        return dp[n];
     }
 };
