67f4491612e8873e0e70e5ed8f9e34683db2af1c
chandelnitin <128496532+chandelnitin@users.noreply.github.com>
2026-07-23 20:41:54 +0530

    Time: 931 ms (33.16%) | Memory: 499.4 MB (67.75%) - LeetSync

--- 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
+++ 3986-maximum-path-score-in-a-grid/maximum-path-score-in-a-grid.cpp
@@ -11,13 +11,15 @@ public:
         if(i>=grid.size()) return INT_MIN;
         if(j>=grid[0].size()) return INT_MIN;
         
-        cost+= (grid[i][j]==0)?0:1;
-        if(cost>k) return INT_MIN;
-        if(dp[i][j][cost]!=-1) return dp[i][j][cost];
-       int right=grid[i][j]+fun(i,j+1,grid,k,cost,dp);
-       int down= grid[i][j]+ fun(i+1,j,grid,k,cost,dp);
+        if(cost>k) return INT_MIN;  // agr ye yhha na ho to out of bund ho jayega dp me cost
+        
+        if(dp[i][j][cost]!=-1) return dp[i][j][cost];  // old cost ke liye agr value nhi mili to old cost ke liye
+                                                        // hi store krenge
+       int newCost=cost+ ((grid[i][j]==0)?0:1);
+       int right=grid[i][j]+fun(i,j+1,grid,k,newCost,dp);
+       int down= grid[i][j]+ fun(i+1,j,grid,k,newCost,dp);
 
-       return dp[i][j][cost]= max(right,down);
+       return dp[i][j][cost]= max(right,down); // old cost ke liye hi store kri value
     }
     int maxPathScore(vector<vector<int>>& grid, int k) {
 vector<vector<vector<int>>> dp(grid.size(),vector<vector<int>>(grid[0].size(),vector<int>(k + 1, -1) ));
